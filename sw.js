9511528e9eecc0baf836bca9edeea45f1c847ae5
i_p_addr@yahoo.com <i_p_addr@yahoo.com>
2018-05-08 13:23:20 +0700

    update service worker

--- sw.js
+++ sw.js
@@ -1,43 +1,60 @@
-const staticCacheName = 'mvs-1-1';
-const urlsToCache = [
-'/',
-'/index.html',
-'/restaurant.html',
-'/css/styles.css',
-'/data/restaurants.json',
-'/img/1.jpg',
-'/img/10.jpg',
-'/img/2.jpg',
-'/img/3.jpg',
-'/img/4.jpg',
-'/img/5.jpg',
-'/img/6.jpg',
-'/img/7.jpg',
-'/img/8.jpg',
-'/img/9.jpg',
-'/js/dbhelper.js',
-'/js/main.js',
-'/js/restaurant_info.js'
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'mws-dataCacheName-v1';
+var cacheName = 'mws-cacheName-v1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/10.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js'
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(caches.open(staticCacheName).then(function(cache){
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-    }));
-})
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('mvs-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
   );
   /*
@@ -53,22 +70,35 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-
-self.addEventListener('fetch', function(event) {
-  // TODO: respond to requests for the root page with
-  // the page skeleton from the cache
-  var requestUrl = new URL(event.request.url);
-
-  if (requestUrl.origin === location.origin) {
-    if (requestUrl.pathname === '/' ) {
-        event.respondWith(cache.match('/'));
-        return;
-    }
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = e.request.url.pathname;
+  if (urlsToCache.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
   }
-
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
 });
